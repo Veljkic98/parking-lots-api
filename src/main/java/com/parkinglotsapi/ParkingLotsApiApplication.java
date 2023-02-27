a6469903bed8aef159aa58c22a6901a12598f082
Veljko Plecas <veljko.plecas@nqode.io>
2023-02-27 18:40:53 +0100

    init app

---
+++ src/main/java/com/parkinglotsapi/ParkingLotsApiApplication.java
@@ -0,0 +1,13 @@
+package com.parkinglotsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLotsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parkinglotsapi/ParkingLotsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkinglotsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
